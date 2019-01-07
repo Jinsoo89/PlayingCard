d017e25fa963a2a26e700e856f6ae2a568016c78
Jinsoo Choi <hello.python89@gmail.com>
2019-01-07 14:48:13 +0900

    update PlayingCard to inherit CustomStringConvertible to display String better

--- PlayingCard/PlayingCard/PlayingCard.swift
+++ PlayingCard/PlayingCard/PlayingCard.swift
@@ -8,11 +8,21 @@
 
 import Foundation
 
-struct PlayingCard {
+// inherits CustomStringConvertible protocol to display string
+struct PlayingCard : CustomStringConvertible {
+    
+    var description: String {
+        return "\(rank)\(suit)"
+    }
+    
     var suit: Suit
     var rank: Rank
 
-    enum Suit: String {
+    enum Suit: String, CustomStringConvertible {
+        var description: String {
+            return rawValue
+        }
+        
         case spades = "♠︎"
         case hearts = "♥︎"
         case diamonds = "♦︎"
@@ -21,7 +31,15 @@ struct PlayingCard {
         static var all = [Suit.spades, .hearts, .diamonds, .clubs]
     }
     
-    enum Rank {
+    enum Rank: CustomStringConvertible {
+        var description: String {
+            switch self {
+            case .ace: return "A"
+            case .numeric(let pips): return String(pips)
+            case .face(let kind): return kind
+            }
+        }
+        
         case ace
         case face(String)
         case numeric(Int)
